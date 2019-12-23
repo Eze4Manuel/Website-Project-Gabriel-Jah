f7e622fe9ae960989c8a4a868f4c0f53acd70835
Eze4Manuel <emmanuelezenagu5@gmail.com>
2019-12-23 15:27:52 +0100

    Styling the header with the text (Powered by sights and sounds) in the navigation file at class=section2

---
+++ application/views/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Home-Gabriel Jah - Creativity is a department of Godliness</title>
+
+    <link rel="stylesheet" href="<?php echo base_url()?>assets/css/indexstyle.css">
+
+  </head>
+  <body>
+    <section class="homepage" style="background-image: url('<?php echo base_url() ?>assets/image/background/BG.jpg')">
+
+    </section>
+
+  </body>
+</html>
